a3e012b0d0608d25950b17899467de41f61b4933
elianamendez1 <elianamendez1809@gmail.com>
2022-04-11 12:57:37 -0500

    SPA and CSS merge

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,18 @@ import { onNavigate } from '../main.js';
 export const Home = () => {
   const LogInDivs = document.createElement('div');
   const containerLogo = `
-  <figure class= "containerLogo">
-    <img src="Imagenes/Logotipo/Logo-icon.png" alt="Binge Worthy logo" class="topLogo">
-  </figure>
-  <figure class="containerLogoLetters">
-    <img src="Imagenes/Logotipo/Logo-letters-only.png" alt="Binge Worthy logo" class="appName">
-    <p class="pink w7">MATCH WITH YOUR STORIES</p>
-  </figure>
-  <button id="signUpBtn" class="button">Create an account</button>
-  <div id="goToLogin">
-    <p>Already have an account?</p>
-    <a id="goToLoginBtn" class="pink w5">Log in</a>
-  </div>`;
+    <figure class= "containerLogo">
+      <img src="Imagenes/Logotipo/Logo-icon.png" alt="Binge Worthy logo" class="topLogo">
+    </figure>
+    <figure class="containerLogoLetters">
+      <img src="Imagenes/Logotipo/Logo-letters-only.png" alt="Binge Worthy logo" class="appName">
+      <p class="pink w7">MATCH WITH YOUR STORIES</p>
+    </figure>
+    <button id="signUpBtn" class="button">Create an account</button>
+    <div id="goToLogin">
+      <p>Already have an account?</p>
+      <a id="goToLoginBtn" class="pink w5">Log in</a>
+    </div>`;
 
   LogInDivs.innerHTML = containerLogo;
   const createAccBtn = LogInDivs.querySelector('#signUpBtn');

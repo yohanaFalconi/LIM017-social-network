bb5c10c5e61616d097321fc77be7d59c187b0edf
Megan Palacios <meg2086@outlook.com>
2022-04-19 11:13:40 -0500

    final style for mobile

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,8 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import { onNavigate } from '../src/main.js';
 
-describe('myFunction', () => {
+describe('onNavigate', () => {
   it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+    expect(typeof onNavigate).toBe('function');
   });
 });
